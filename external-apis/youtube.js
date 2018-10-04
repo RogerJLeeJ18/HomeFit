c340860078e527c04f23cdbb11c45da95f5798ee
Roger Journee <rogerjournee@gmail.com>
2018-10-04 09:34:24 -0500

    edited youtube service

--- external-apis/youtube.js
+++ external-apis/youtube.js
@@ -1,20 +1,24 @@
 const axios = require('axios');
-const { YOUTUBE_API_KEY } = require('config.js');
-module.exports = (workoutUrl, callback) => {
-    axios.get('https://www.googleapis.com/youtube/v3/playlists', {
+const { YOUTUBE_API_KEY } = require('../config.js');
+const getVideo = (workoutUrl, callback) => {
+    // const id = workoutUrl.slice(30);
+    const obj = { 
         params: {
             part: 'snippet',
-            id: PLKrfb-haFwJgKpDLFintPMyBHUrJW99tx,
-            maxResults: 50,
+            id: workoutUrl,
             key: YOUTUBE_API_KEY
         }
-    })
-    .then(function ({data}) {
-        // call the callback on the resulting list of videos from the playlist
-    })
-    .catch(function({data}){
-        data.error.errors.forEach(function(err) {
-            console.error(err.message);
-        })
-    })
+    }
+    return axios.get('https://www.googleapis.com/youtube/v3/videos', obj)
+    // .then(({ data }) => {
+    //     // call the callback on the resulting list of videos from the playlist
+    //     // console.log(data.items);
+    //     const [ item ] = data.items;
+    //     console.log(item);
+    // })
+    // .catch(err => {
+    //     console.error(err);
+    // })
 };
+
+module.exports = getVideo;
